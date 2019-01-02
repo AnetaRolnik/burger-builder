5cd3b48a19281612d8c807b6c511f00baaeebc45
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-01-02 19:17:15 +0100

    Add the order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,21 @@ class BurgerBuilder extends Component {
                 meat: 0,
             },
             totalPrice: 4,
+            purchasable: false,
         }
+    }
+
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey]
+            })
+            .reduce((sum,el) => {
+                return sum+el;
+            }, 0);
+        this.setState({
+            purchasable: sum>0
+        });
     }
 
     addIngredientHandler = (type) => {
@@ -40,6 +54,7 @@ class BurgerBuilder extends Component {
             ingredients: updatedIngredients,
             totalPrice: newPrice,
         });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -60,6 +75,7 @@ class BurgerBuilder extends Component {
             ingredients: updatedIngredients,
             totalPrice: newPrice,
         });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render() {
@@ -76,6 +92,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
                 />
             </Auxiliary>
