0df313667b9281d1dd42973cd943a2be3063ff8a
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-01-21 18:46:22 +0100

    Replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const app = (
-    <BrowserRouter>
+    <HashRouter>
         <App/>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
