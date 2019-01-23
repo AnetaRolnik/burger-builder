f67c617513a718bc60a7fb2bca98ef4a90f7e850
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-01-23 17:38:21 +0100

    Dynamically create inputs based on JS Config

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,12 +86,23 @@ class ContactData extends Component {
     };
 
     render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
             <form>
-                <Input elementType/>
-                <Input />
-                <Input />
-                <Input />
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}/>
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
