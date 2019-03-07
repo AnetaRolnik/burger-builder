2348b3ebb977d52085cb6433703091711f8c969f
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-03-07 19:56:32 +0100

    Manage ingredients and price through Redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 const app = (
-    <HashRouter>
-        <App/>
-    </HashRouter>
+    <Provider store={store}>
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
